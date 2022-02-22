a4617b3ef94b02e818f56431c4f65c72a26fc833
Pranaav Rao <raop@vcu.edu>
2022-02-21 23:47:55 -0500

    update

--- ngram.py
+++ ngram.py
@@ -1,5 +1,16 @@
 from sys import argv
+import re
 
 ngram = argv[1]
 numSentences = argv[2]
-files = argv[3:]
+filenames = argv[3:]
+text = ''
+
+#this loop iterates over the arguments of the program, accesses the named files, and adds all text to the corpus string
+for file in filenames:
+    with open(file, encoding="utf8") as f:
+        text += f.read()
+
+text = re.sub(r'[.|!|?]', '<end> <start>',text)
+
+
